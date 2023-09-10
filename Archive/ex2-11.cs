f74ba4068682f2e2916d161573c8be9590a851a9
4xvgal <4xvgal@protonmail.com>
2023-09-10 16:20:37 +0900

    [update] ex2-11.cs edit

--- Archive/ex2-11.cs
+++ Archive/ex2-11.cs
@@ -1,7 +1,7 @@
 ﻿//ex2-11.cs
 using System;
 using System.Globalization;
-
+//가변 길이 배열 만들기
 class ArrayOfArrayApp
 {
     public static void Main()
@@ -12,10 +12,11 @@ class ArrayOfArrayApp
         {
             arrayOfArray[i] = new int[i + 3];
         }
-        for (i = 0; i < arrayOfArray.Length; i++)
+        for (i = 0; i < arrayOfArray.Length; i++) // 배열에 0 1 2, 4 5 6 7, 10 11.. 순으로 채우기
             for (j = 0; j < arrayOfArray[i].Length; j++)
                 arrayOfArray[i][j] = i * arrayOfArray.Length + j;
-        for (i = 0; i < arrayOfArray.Length; i++)
+
+        for (i = 0; i < arrayOfArray.Length; i++) //배열 출력하기
         {
             for (j = 0; j < arrayOfArray[i].Length; j++)
                 Console.Write(" " + arrayOfArray[i][j]);
