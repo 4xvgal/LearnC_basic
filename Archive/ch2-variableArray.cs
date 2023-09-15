4ce38c59a6109a4afd511bb3e6eb3f5683f32292
4xvgal <4xvgal@protonmail.com>
2023-09-15 11:46:20 +0900

    [UPDATE] 2장 내용 추가

--- Archive/ch2-variableArray.cs
+++ Archive/ch2-variableArray.cs
@@ -8,19 +8,19 @@ class variableArray
         //동적 할당 배열 생성
         int[,] num = new int[2, 3];
         //배열 값 저장
-        for(int i = 0; i < 2; i++)
-        {
-            for(int j =0;j < 3; j++)
-            {
-                num[i,j] = i;
-            }
-        }
-        //배열 값 출력
-        for(int i = 0;i< 2; i++)
+        for (int i = 0; i < 2; i++)
         {
             for (int j = 0; j < 3; j++)
             {
-                Console.Write("{0}  ", num[i,j]);
+                num[i, j] = i;
+            }
+        }
+        //배열 값 출력
+        for (int i = 0; i < 2; i++)
+        {
+            for (int j = 0; j < 3; j++)
+            {
+                Console.Write("{0}  ", num[i, j]);
             }
             Console.WriteLine();
         }
---
+++ Archive/ex2-29.cs
@@ -0,0 +1,21 @@
+﻿//ex2-29.cs
+using System;
+//박싱 언박싱 예제
+class BoxingUnboxingApp
+{
+    public static void Main()
+    {
+        int foo = 526;
+        object bar = foo;
+        Console.WriteLine(bar);
+        try
+        {
+            double d = (int)bar;
+            Console.WriteLine(d);
+        }
+        catch (InvalidCastException e)
+        {
+            Console.WriteLine(e + "Error");
+        }
+    }
+}
